da2423fe24d7291eb9a21520d69eb512c6d0b707
Bratan <alfabrayan12@gmail.com>
2020-12-22 16:22:25 -0600

    32: Agregar : al mensaje de error

--- src/SemanticErrorManager/UnexpectedTypeException.java
+++ src/SemanticErrorManager/UnexpectedTypeException.java
@@ -3,7 +3,7 @@ package SemanticErrorManager;
 public class UnexpectedTypeException extends SemanticException{
 
     public UnexpectedTypeException(int line, int caracter, String esperado, String recibido) {
-        super("línea " + line + ":" + caracter + " : tipo de dato erróneo, tipo esperado: " + esperado + " recibido "+ recibido);
+        super("línea " + line + ":" + caracter + " : tipo de dato erróneo, tipo esperado: " + esperado + " recibido: "+ recibido);
     }
 
 
